7ce930a9bf14a34946377526c891cd255350a4b9
root <jns.klr@gmail.com>
2018-05-01 04:12:21 -0300

    muitos para muitos

--- src/main/java/com/aula/TesteAulaApplication.java
+++ src/main/java/com/aula/TesteAulaApplication.java
@@ -8,14 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.aula.domin.Categoria;
+import com.aula.domin.Produto;
 import com.aula.repositories.CategoriaRepository;
+import com.aula.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class TesteAulaApplication implements CommandLineRunner {
 
 	@Autowired
-	private CategoriaRepository repo;
-	
+	private CategoriaRepository cRepo;
+	@Autowired
+	private ProdutoRepository pRepo;
+
 	public static void main(String[] args) {
 		SpringApplication.run(TesteAulaApplication.class, args);
 	}
@@ -26,7 +30,19 @@ public class TesteAulaApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null, "informática");
 		Categoria cat2 = new Categoria(null, "escritório");
 
-		repo.saveAll(Arrays.asList(cat1, cat2));
-		
+		Produto p1 = new Produto(null, "computador", 3000.00);
+		Produto p2 = new Produto(null, "impressora", 800.00);
+		Produto p3 = new Produto(null, "mouse", 20.00);
+
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+
+		cRepo.saveAll(Arrays.asList(cat1, cat2));
+		pRepo.saveAll(Arrays.asList(p1, p2, p3));
+
 	}
 }
