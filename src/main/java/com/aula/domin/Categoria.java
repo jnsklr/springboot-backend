f6c5273173a037fcff106803eca9ef2869616444
root <jns.klr@gmail.com>
2018-05-01 03:07:03 -0300

    jpa base de dados

--- src/main/java/com/aula/domin/Categoria.java
+++ src/main/java/com/aula/domin/Categoria.java
@@ -2,12 +2,18 @@ package com.aula.domin;
 
 import java.io.Serializable;
 
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 
@@ -62,6 +68,4 @@ public class Categoria implements Serializable {
 		return true;
 	}
 
-	
-	
 }
