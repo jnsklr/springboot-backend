2ed73705c17211664c2700f887455326715aa38e
root <jns.klr@gmail.com>
2018-05-01 02:39:03 -0300

    testando primeira classe de domin categoria

--- src/main/java/com/aula/resources/CategoriaResources.java
+++ src/main/java/com/aula/resources/CategoriaResources.java
@@ -1,15 +1,28 @@
 package com.aula.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.aula.domin.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResources {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "informatica");
+		Categoria cat2 = new Categoria(1, "escretorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
