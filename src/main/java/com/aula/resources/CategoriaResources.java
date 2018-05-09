57f6d7fad5ecd45d1efc43e09e8effb84ea555c2
root <jns.klr@gmail.com>
2018-05-09 19:07:26 -0300

    DTO

--- src/main/java/com/aula/resources/CategoriaResources.java
+++ src/main/java/com/aula/resources/CategoriaResources.java
@@ -1,6 +1,11 @@
 package com.aula.resources;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.cookie;
+
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.aula.domin.Categoria;
+import com.aula.dto.CategoriaDTO;
 import com.aula.services.CategoriaService;
 
 @RestController
@@ -50,5 +56,13 @@ public class CategoriaResources {
 		service.delete(id);
 
 		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 }
