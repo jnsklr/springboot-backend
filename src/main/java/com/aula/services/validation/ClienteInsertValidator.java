344e35d02db3757a2aedf9cc44543afbc415a908
root <jns.klr@gmail.com>
2018-05-16 11:41:51 -0300

    email não repido

--- src/main/java/com/aula/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/aula/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.aula.domin.Cliente;
 import com.aula.domin.enuns.TipoCliente;
 import com.aula.dto.ClienteNewDTO;
+import com.aula.repositories.ClienteRepository;
 import com.aula.resources.exception.FieldMessage;
 import com.aula.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+	@Autowired
+	private ClienteRepository repoCli;
+
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -27,6 +35,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ invalido"));
+		}
+
+		Cliente aux = repoCli.findByEmail(objDto.getEmail());
+
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existe"));
 		}
 
 		for (FieldMessage e : list) {
