6022b9fb01e388023a274f9a85902a9c8cf7dc84
root <jns.klr@gmail.com>
2018-05-16 14:07:33 -0300

    total e subtotal

--- src/main/java/com/aula/domin/ItemPedido.java
+++ src/main/java/com/aula/domin/ItemPedido.java
@@ -33,6 +33,11 @@ public class ItemPedido implements Serializable{
 		this.quantidade = quantidade;
 		this.preco = preco;
 	}
+	
+	public double getSubtotal() {
+		return (preco - desconto) * quantidade;
+	}
+	
 
 	@JsonIgnore
 	public Pedido getPedido() {
